5b58819939e9a25939b5fbfa90d82857c556f98e
anlandong <ad3230@nyu.edu>
2019-04-24 16:17:08 -0400

    TensorFlow Lite Image Classifier integrated.

--- app/src/main/java/com/example/healtheye/View/MainActivity.java
+++ app/src/main/java/com/example/healtheye/View/MainActivity.java
@@ -1,19 +1,21 @@
 package com.example.healtheye.View;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.ViewModelProviders;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 
 import com.example.healtheye.R;
 import com.example.healtheye.ViewModel.AccountViewModel;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.example.healtheye.liteClassifier.*;
+
 
 public class MainActivity extends AppCompatActivity {
     private AccountViewModel accountViewModel;
+    private FloatingActionButton cameraButton;
     private FloatingActionButton searchButton;
 
     @Override
@@ -22,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //accountViewModel = ViewModelProviders.of(this).get(AccountViewModel.class);
-        searchButton = findViewById(R.id.button_searchFoodMain);
+        searchButton = findViewById(R.id.button_activityMain_SearchFood);
         searchButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
@@ -31,5 +33,17 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(searchIntent);
             }
         });
+
+        cameraButton = findViewById(R.id.button_activityMain_Camera);
+        cameraButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent cameraIntent = new Intent(MainActivity.this, ClassifierActivity.class);
+                startActivity(cameraIntent);
+                Log.d("Camera", "Camera Clicked");
+
+            }
+        });
+
     }
 }
